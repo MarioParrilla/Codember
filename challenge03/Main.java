4a15005f76701f881783dd1849d082a9342f4808
MarioParrilla <marioparrilla2@gmail.com>
2022-11-28 11:13:17 +0000

    Challenge03 Completed

---
+++ challenge03/Main.java
@@ -0,0 +1,60 @@
+package challenge03;
+
+import challenge02.utils.ASCIIUtils;
+import io.Input;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    final static String path = "/challenge03/colors.txt";
+    public static void main(String[] args) {
+        try {
+            run(Paths.get("").toAbsolutePath()+path);
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    static void run(String path) throws IOException {
+        Input.init(path);
+        List<String> listOfColors;
+        ArrayList<String> zebra = new ArrayList<>();
+        do {
+            listOfColors = Input.readLinesToList();
+            if (listOfColors == null)
+                break;
+
+            listOfColors = listOfColors.stream().map( e -> e.trim().replaceAll("\",|\"","")).toList();
+
+            String lastColor = null;
+            int maxCount = 0;
+
+            for (String color : listOfColors) {
+                if (zebra.size() == 1 && zebra.get(0).equals(color))
+                    continue;
+
+                if (zebra.size() < 2 || zebra.get(zebra.size()-2).equals(color)) {
+                    zebra.add(color);
+                    continue;
+                }
+
+                if (zebra.size() >= maxCount) {
+                    lastColor = zebra.get(zebra.size()-1);
+                    maxCount = zebra.size();
+                }
+                zebra = new ArrayList<>(List.of(zebra.get(zebra.size()-1), color));
+            }
+            if (zebra.size() >= maxCount) {
+                lastColor = zebra.get(zebra.size()-1);
+                maxCount = zebra.size();
+            }
+
+            System.out.printf("%d, %s\n", maxCount, lastColor);
+        } while (true);
+    }
+
+}
--- io/Input.java
+++ io/Input.java
@@ -1,6 +1,8 @@
 package io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
     private static File file = null;
@@ -22,9 +24,23 @@ public class Input {
                 lines += line;
             else
                 lines += " "+line;
+            System.out.println(line);
         }
 
         if (!lines.equals("")) return lines;
+
+        return null;
+    }
+
+    public static <T> List<T> readLinesToList() throws IOException {
+        List<T> list = new ArrayList<>();
+        String line = null;
+
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            list.add((T) line);
+        }
+
+        if (!list.isEmpty()) return list;
 
         return null;
     }
